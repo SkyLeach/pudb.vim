25ebaeb00e82027e68b1d0649ed869d8a60c7023
Jeunghun Kim <nanan100410@gmail.com>
2019-04-23 23:46:36 +0900

    Fix PUDBClearAllBreakpoints command
    Fix command
    Before: remove all sign
    After: remove only pudb.vim sign(name: pudbbp)

--- rplugin/python3/vim_pudb.py
+++ rplugin/python3/vim_pudb.py
@@ -178,7 +178,10 @@ class NvimPudb(object):
         """
         if not buffname:
             buffname = self.cbname()
-        self.nvim.command('sign unplace * file={}'.format(buffname))
+        for key in self._bps_placed:
+            for i in self._bps_placed[key]:
+                self.nvim.command('sign unplace {} file={}'.format(i,
+                                                                   buffname))
         self._bps_placed[buffname] = []
         self.update_pudb_breakpoints(buffname)
 
