54fbe146bb2bf3a35b1125d5d8b0cd5803d7bff5
Jeunghun Kim <nanan100410@gmail.com>
2022-11-11 14:37:27 +0900

    Update methods
    Remove update_pudb_breakpoints, update_buffer
    Fix place_sign, remove_sign, has_breakpoint
    Add test_buffer, load_bp_file, save_bp_file, blank_file, make_links
    Add load_base_dir, find_bp_files, print_feature

--- rplugin/python3/vim_pudb.py
+++ rplugin/python3/vim_pudb.py
@@ -125,49 +125,6 @@ class NvimPudb(object):
         __logger__.addHandler(nvimhandler)
         # define our sign command
 
-    def place_sign(self, buffname, lineno):
-        """place_sign
-        Places a new sign on the buffer and registers it as a breakpoint with
-        pudb
-        :param buffname:
-        :param lineno:
-        """
-        # do nothing without a line number
-        if not lineno:
-            return None
-        # don't place it if it has already been placed
-        if self.has_breakpoint(buffname, lineno):
-            return self.pudbbp(buffname, lineno)
-        if self.toggle_sign:
-            signcmd = "sign place {} line={} name={} file={}".format(
-                signid(buffname, lineno),
-                lineno,
-                self.sgnname(),
-                buffname)
-            self.nvim.command(signcmd)
-        if buffname in self._bps_placed:
-            self._bps_placed[buffname].append(signid(buffname, lineno))
-        else:
-            self._bps_placed[buffname] = [signid(buffname, lineno)]
-        return self.pudbbp(buffname, lineno)
-
-    def remove_sign(self, buffname, lineno):
-        """remove_sign
-        removes the sign from the buffer and the breakpoint from pudb
-        :param buffname:
-        :param bpt:
-        """
-        if not self.has_breakpoint(buffname, lineno):
-            return
-        if self.toggle_sign:
-            vimmsg = 'sign unplace {} file={}'.format(
-                signid(buffname, lineno),
-                buffname)
-            self.nvim.command(vimmsg)
-        self._bps_placed[buffname].pop(
-            self._bps_placed[buffname].index(
-                signid(buffname, lineno)))
-
     @neovim.command("PUDBClearAllBreakpoints", sync=False)
     def clear_all_bps(self, buffname=None):
         if not buffname:
@@ -369,3 +326,112 @@ class NvimPudb(object):
             return
         self.buf_initial(buffname)
         self.update_breakpoints_cmd(buffname)
+
+    def test_buffer(self, buffname):
+        if buffname not in self._bps_placed:
+            self._bps_placed[buffname] = []
+        if buffname not in self._toggle_status:
+            self._toggle_status[buffname] = False
+
+    def load_bp_file(self):
+        self.make_links()
+        self._bps_placed = {}
+        self._cond_dict = {}
+
+        lines = []
+        with open(self._bp_file, "r") as f:
+            lines.extend([line.strip() for line in f.readlines()])
+
+        for line in lines:
+            buffname, num_line = line[2:].split(':')
+            num_line = num_line.split(', ')
+
+            if len(num_line) > 1:
+                self._cond_dict["{}:{}".format(
+                        buffname, num_line[0])] = ", ".join(num_line[1:])
+
+            if buffname in self._bps_placed:
+                self._bps_placed[buffname].append(int(num_line[0]))
+            else:
+                self._bps_placed[buffname] = [int(num_line[0])]
+            self._bps_placed[buffname] = list(set(self._bps_placed[buffname]))
+            self._bps_placed[buffname].sort()
+
+            if buffname not in self._toggle_status:
+                self._toggle_status[buffname] = False
+
+        self.test_buffer(self.cbname())
+
+    def save_bp_file(self):
+        self.make_links()
+        with open(self._bp_file, "w") as f:
+            for buffname in sorted(self._bps_placed):
+                self._bps_placed[buffname] = list(set(self._bps_placed[buffname]))
+                self._bps_placed[buffname].sort()
+                for num_line in self._bps_placed[buffname]:
+                    cond_line = "{}:{}".format(buffname, num_line)
+                    if cond_line in self._cond_dict:
+                        f.write('b {}:{}, {}\n'.format(buffname,
+                                                       num_line,
+                                                       self._cond_dict[cond_line]))
+                    else:
+                        f.write('b {}:{}\n'.format(buffname, num_line))
+
+    def blank_file(self):
+        if not os.path.exists(self._bp_file):
+            with open(self._bp_file, "w") as f:
+                pass
+
+    def make_links(self):
+        self.blank_file()
+        for bp_file in self.find_bp_files():
+            tmp_path = self._bp_config_dir + bp_file
+            if not os.path.islink(tmp_path):
+                with open(tmp_path, "r") as f:
+                    tmp_input = f.read()
+                with open(self._bp_file, "a") as f:
+                    f.write(tmp_input)
+                os.remove(tmp_path)
+                os.symlink(self._bp_file, tmp_path)
+
+    def load_base_dir(self):
+        _home = os.environ.get("HOME", os.path.expanduser("~"))
+        xdg_config_home = os.environ.get(
+            "XDG_CONFIG_HOME",
+            os.path.join(_home, ".config") if _home else None)
+
+        if xdg_config_home:
+            xdg_config_dirs = [xdg_config_home]
+        else:
+            xdg_config_dirs = os.environ.get("XDG_CONFIG_DIRS", "/etc/xdg").split(":")
+        self._bp_config_dir = xdg_config_dirs[0] + "/pudb/"
+        self._bp_file = xdg_config_dirs[0] + "/pudb/saved-breakpoints"
+
+    def find_bp_files(self):
+        files = []
+        breakpoints_file_name = "saved-breakpoints-"
+        for entry in os.listdir(self._bp_config_dir):
+            if breakpoints_file_name in entry:
+                files.append(entry)
+        return sorted(list(set(files)))
+
+    def place_sign(self, buffname, lineno):
+        signcmd = "sign place {} line={} name={} file={}".format(
+            lineno * 10, lineno, self.sgnname(), buffname)
+        self.nvim.command(signcmd)
+
+    def remove_sign(self, buffname, lineno):
+        signcmd = 'sign unplace {} file={}'.format(
+            lineno, buffname)
+        self.nvim.command(signcmd)
+
+    def has_breakpoint(self, buffname, lineno):
+        self.test_buffer(buffname)
+        if lineno in self._bps_placed[buffname]:
+            return True
+        return False
+
+    def print_feature(self, print_input):
+        print_feature = 'echo "{}"'.format(pprint.pformat(print_input))
+        self.nvim.command(print_feature)
+
